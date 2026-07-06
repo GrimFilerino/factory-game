e45f5c9dba77e016f7cce42436e551d0a0e28545
Grimfilerino <me@grimfilerino.com>
2026-07-06 23:15:15 +0200

    Fixed wrong type for transform component

--- src/entities/components/transform/transform.c
+++ src/entities/components/transform/transform.c
@@ -1,6 +1,6 @@
 #include "transform.h"
 
-void applyVelocityToPosition(Transform* transform){
+void applyVelocityToPosition(transform_t* transform){
         *transform->position = add_vector(*transform->position, *transform->velocity);
         set_vector_to_constant(transform->velocity, 0);
 }
