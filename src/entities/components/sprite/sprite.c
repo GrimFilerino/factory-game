b47dd37bf7c7a69a70650bc10c446cfa25f828da
Grimfilerino <me@grimfilerino.com>
2026-07-12 23:53:27 +0200

    Fixed so sprite now creates a new texture and draws it on position
    Will fix part of texture later

--- src/entities/components/sprite/sprite.c
+++ src/entities/components/sprite/sprite.c
@@ -1,1 +1,48 @@
 #include "sprite.h"
+#include <raylib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../../hashmap/hashmap.h"
+
+Hashmap* textures;
+
+unsigned int add_texture(char* filename) {
+        Texture2D* texture = malloc(sizeof(Texture2D));
+        if(!texture) {
+                return 0;
+        }
+
+        if(textures == NULL) {
+                textures = create_hash_map();
+        }
+
+        printf("Loading: %s\n", filename);
+        *texture = LoadTexture(filename);
+
+        if (texture->id == 0) {
+                printf("Failed to load texture: %s\n", filename);
+                free(texture);
+                return 0;
+        }
+
+        char* str_id = calloc(256, sizeof(char));
+        sprintf(str_id, "%d", texture->id);
+
+        hash_map_add(textures, str_id, (void*)texture);
+
+        return texture->id;
+}
+
+
+void draw_sprite(sprite_t *sprite, vec2_t *pos) {
+        char str_id[256];
+        sprintf(str_id, "%d", sprite->texture_id);
+        Texture2D* texture = hash_map_get(textures, str_id);
+
+        if (texture == NULL) {
+                printf("Texture %u not found!\n", sprite->texture_id);
+                return;
+        }
+
+        DrawTexture(*texture, pos->x, pos->y, WHITE);
+}
--- src/entities/components/sprite/sprite.h
+++ src/entities/components/sprite/sprite.h
@@ -6,11 +6,11 @@
 typedef struct sprite {
         vec2_t size;
         vec2_t frame_position;
-        unsigned short texture_id;
         unsigned short animation_index; 
+        unsigned int texture_id;
 } sprite_t;
 
-sprite_t* create_sprite(vec2_t size, unsigned short texture_id);
+unsigned int add_texture(char* filename);
 
 void draw_sprite(sprite_t* sprite, vec2_t* pos);
 
