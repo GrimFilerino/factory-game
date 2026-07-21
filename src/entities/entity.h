f6b5fe452ecc16afc632370dcfa18d0b3721bc05
Grimfilerino <me@grimfilerino.com>
2026-07-21 23:50:09 +0200

    Updated to bools instead and removed ANT entity and added WALL as a temp entity to test stuff on

--- src/entities/entity.h
+++ src/entities/entity.h
@@ -1,21 +1,23 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+#include <stdbool.h>
 
 typedef enum entities {
         PLAYER,
-        ANT
+        WALL 
 } entities_t;
 
 typedef struct entity {
         entities_t type;
         char* id;
-        unsigned is_alive:1;
-        unsigned has_transform:1;
-        unsigned has_sprite:1;
-        unsigned has_health:1;
-        unsigned has_input:1;
-        unsigned has_camera:1;
+        bool is_alive;
+        bool has_transform;
+        bool has_sprite;
+        bool has_health;
+        bool has_input;
+        bool has_aabb;
+        bool has_damage;
 } entity_t;
 
 #endif
