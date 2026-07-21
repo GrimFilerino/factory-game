2aeb39415be6212e0ad1ab164d1c83c455fe38dd
Grimfilerino <me@grimfilerino.com>
2026-07-21 02:59:34 +0200

    Removed unused function in transform component

--- src/entities/components/transform/transform.c
+++ src/entities/components/transform/transform.c
@@ -1,7 +1,1 @@
 #include "transform.h"
-
-void applyVelocityToPosition(transform_t* transform){
-        *transform->position = add_vector(*transform->position, *transform->velocity);
-        set_vector_to_constant(transform->velocity, 0);
-}
-
--- src/entities/components/transform/transform.h
+++ src/entities/components/transform/transform.h
@@ -2,15 +2,10 @@
 #define TRANSFORM_H
 
 #include "../../../math/vector.h"
+
 typedef struct transfrom {
         vec2_t* velocity;
         vec2_t* position;
 } transform_t;
 
-
-void applyVelocityToPosition(transform_t* transform);
-
 #endif
-
-
-
